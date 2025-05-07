01e84a9a0de6ca6a79b2063bd6ca9b516a67169b
Riya Karnwal <riyakarnwal2004@gmail.com>
2025-05-08 01:50:26 +0530

    learnind redux toolkit

--- reactLearn/day7-of-react/src/App.jsx
+++ reactLearn/day7-of-react/src/App.jsx
@@ -1,14 +1,28 @@
-
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement,incrementByAmount, reset } from './features/counterSlice';
 import './App.css'
+import UserList from './UserList';
 
 function App() {
-
-
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+  const [Amount,setAmount]=useState(0);
+  function handleincrement(){
+    dispatch(incrementByAmount(Amount));
+  }
+  function handleReset(){
+    dispatch(reset());
+  }
   return (
     <>
-     
-        
-    </>
+     <h1>Count: {count}</h1>
+      <button onClick={() => dispatch(increment())}>+1</button>
+      <button onClick={() => dispatch(decrement())}>-1</button>
+       <input type="Number" value={Amount} placeholder='Enter Amount' onChange={(e)=>setAmount(e.target.value)}/> 
+       <button onClick={handleincrement}>Inc By amount</button>
+       <button onClick={handleReset}>Reset</button>
+       </>
   )
 }
 
--- reactLearn/day7-of-react/src/main.jsx
+++ reactLearn/day7-of-react/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+  <App />
+</Provider>
 )
